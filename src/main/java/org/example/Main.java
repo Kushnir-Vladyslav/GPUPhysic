90467d7a4ffc7647c5a707876b6710099dc35ff7
Влад Кушнір <vlad987321@gmail.com>
2025-02-12 00:20:34 +0200

    fix several bags and some refactor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,37 +1,13 @@
 package org.example;
 
 
-import org.example.FileFinder.ClassFinder;
-import org.example.FileFinder.LibraryFinder;
 import org.example.JavaFX.JavaFX;
-import org.example.Library.LibraryManager;
+
 
 public class Main {
     public static void main(String[] args) {
-//        particles = new Particle[256];
-//
-//        for(int i = 0; i < 256; i++) {
-//            particles[i] = new Particle();
-//        }
-
-
-        try {
-            System.out.println(ClassFinder.findClass("StructsLibrary"));
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
-        String text = "lol dva SOME_VALUE";
-
-        text = text.replace("SOME_VALUE", String.valueOf(500.f / 2.56f));
-        System.out.println(text + "f");
-//        System.out.println(LibraryManager.getInstance().getLibrary("Structs"));
 
         JavaFX.main(args);
 
-//        OpenCL oCL = new OpenCL();
-//        oCL.call();
     }
-
-
 }
